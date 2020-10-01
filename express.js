dd485b8d29666f11a1d22c819c42faee3fcd83d8
Michael Griffiths <michael.griffiths@bglgroup.co.uk>
2020-10-01 12:07:55 +0100

    user rest implementations

--- express.js
+++ express.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-const users = [
+let users = [
   {
     id: 1,
     username: 'john',
@@ -21,7 +21,7 @@ const users = [
   }
 ];
 
-const messages = [
+let messages = [
   {
     id: 1,
     from: 1,
@@ -61,11 +61,102 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/users/:id', (req, res) => {
-  const foundUser = users.find(user => user.id === parseInt(req.params.id, 10));
+  const id = parseInt(req.params.id, 10);
+  const foundUser = users.find(user => user.id === id);
   if (!foundUser) {
     return res.sendStatus(404);
   }
   return res.json(foundUser);
+});
+
+app.post('/users', (req, res) => {
+  const { username, fullname } = req.body;
+  if (!username || !fullname) {
+    return res.status(400).send('Missed arguments');
+  }
+
+  if(users.some(user => user.username === username)) {
+    return res.status(400).send('Username already in use');
+  }
+
+  const newUser = {
+    id: new Date().getTime(),
+    username, fullname,
+    created: new Date(),
+  };
+  users.push(newUser);
+
+  res.json(newUser);
+});
+
+app.put('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const { username, fullname } = req.body;
+
+  if (!username || !fullname) {
+    return res.status(400).send('Missed arguments');
+  }
+
+  const foundUserIndex = users.findIndex(user => user.id === id);
+  if (foundUserIndex === -1) {
+    return res.status(400).send('User not found');
+  }
+
+  if(users.some(user => user.id !== id && user.username === username)) {
+    return res.status(400).send('Username already in use');
+  }
+
+  users = [
+    ...users.slice(0, foundUserIndex),
+    {
+      ...users[foundUserIndex],
+      username, fullname,
+    },
+    ...users.slice(foundUserIndex + 1),
+  ]
+
+  res.json(users[foundUserIndex]);
+});
+
+app.patch('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const { username, fullname } = req.body;
+
+  if (!username && !fullname) {
+    return res.status(400).send('Missed arguments');
+  }
+
+  const foundUserIndex = users.findIndex(user => user.id === id);
+  if (foundUserIndex === -1) {
+    return res.status(400).send('User not found');
+  }
+
+  if(username && users.some(user => user.id !== id && user.username === username)) {
+    return res.status(400).send('Username already in use');
+  }
+
+  users = [
+    ...users.slice(0, foundUserIndex),
+    {
+      ...users[foundUserIndex],
+      ...(username && { username }),
+      ...(fullname && { fullname }),
+    },
+    ...users.slice(foundUserIndex + 1),
+  ]
+
+  res.json(users[foundUserIndex]);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  const foundUserIndex = users.findIndex(user => user.id === id);
+  if (foundUserIndex === -1) {
+    return res.status(400).send('User not found');
+  }
+
+  return res.sendStatus(501);
 });
 
 app.get('/messages', (req, res) => {
